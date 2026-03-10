d24d9cd7c3edf9e9389f9255a93a743671bcc8e5
c-pranav-sharma <pranavsharmachouduru@gmail.com>
2026-03-11 03:49:21 +0530

    docs(support): add module-level documentation

--- routes/support.js
+++ routes/support.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express'); // routing module
 const router = express.Router();
 const axios = require('axios');
 const Complaint = require('../models/Complaint');
